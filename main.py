89f1f6204059d7fdfdeaacb13c1f0bfa33bfd01e
michidevall <glitzpixy1@yahoo.com>
2021-12-13 18:36:38 -0700

    second commit

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ def guess(x):
     random_number = random.randint(1,x)
     guess = 0
     while guess != random_number:
-        guess = input('Guess a number between')
+        guess = input('Guess a number between 1 and {x}')
+        
+guess(10)
